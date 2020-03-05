2e1594b3e4909bae83faeee727bcc5d52ac0b68b
Ava Trogus <avatrogus@Avas-MacBook-Pro-4.local>
2020-03-05 11:38:58 -0800

    line graph updates

--- src/updateGraphs.js
+++ src/updateGraphs.js
@@ -1,15 +1,20 @@
-
+const lineGraph = require("./lineGraph.js");
 const piChart = require("./simplePieChart.js");
 const bubbleGraph = require("./bubbleGraph.js");
+
+const lineGraphInstance = new lineGraph();
 const piChartInstance = new piChart();
 const bubbleGraphInstance = new bubbleGraph();
+
 
 var $intentSelector = document.getElementById("intent-select");
 
 function updateAll() {
     console.log("should be here");
+    lineGraphInstance.updateGraph();
     piChartInstance.updatePiChart();
     bubbleGraphInstance.updateGraph();
+
 }
 
 $intentSelector.onchange = function(e) {
